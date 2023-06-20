16e242f40748a6ddb9d49771c05dc50878659181
KashishOO7 <charayakashish@gmail.com>
2023-06-21 00:02:49 +0530

    update

---
+++ assets/web terminal/commands.js
@@ -0,0 +1,65 @@
+var blog = "https://medium.com/@kashishcharaya";
+var notes = "https://notes.learnsec101.com/untitled/welcome/intro";
+// Don't look here mate
+var password = "wargames";
+var linkedin = "https://www.linkedin.com/in/kashish-charaya-/";
+var github = "https://github.com/KashishOO7";
+var email = 'hello@learnsec101.com';
+
+whois = [
+  "<br>",
+  "Hey,",
+  "I'm a security researcher from India",
+  "I created this portfolio site as I was also intersted in Web Dev once and that's what brings me back",
+  "But had to switch to Cyber Security full time because I wanted to",
+  "As, I couldn't do both simultaneously",
+  "I also write about life and philosophy sometimes and even also wrote an eBook",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "Probably Guest",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'Blog        <a href="' + blog + '" target="_blank">' + "</a>",
+  'Notes        <a href="' + notes + '" target="_blank">' + '</a>',
+  'Linkedin       <a href="' + linkedin + '" target="_blank">' + "</a>",
+  'Github         <a href="' + github + '" target="_blank">' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">A hint</span>           Type: "let me in"',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Work in progress... some of them are, on GitHub.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Kashish?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display available socials',
+  '<span class="command">?</span>              World\'s first hacking movie?',
+  '<span class="command">projects</span>       View some projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           That\'s what you typed',
+  '<span class="command">email</span>          Contact me?',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+banner = [
+ "Hello World,",
+ "I'm just another Human born on the planet earth who thought life was absurd.",
+  "<span class=\"color2\">For available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
---
+++ assets/web terminal/main.js
@@ -0,0 +1,180 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function() {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+console.log(
+  "%cSecurity Breached!",
+  "color: #04ff00; font-weight: bold; font-size: 24px;"
+);
+console.log("%cPassword: '" + password + "' - Just Hack it?", "color: grey");
+
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === password) {
+      pwd = true;
+    }
+    if (pwd && e.keyCode == 13) {
+      loopLines(secret, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("password");
+    } else if (e.keyCode == 13) {
+      addLine("Wrong PASSWORD", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("password");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("guest@learnsec101.com:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "whois":
+      loopLines(whois, "color2 margin", 80);
+      break;
+    case "whoami":
+      loopLines(whoami, "color2 margin", 80);
+      break;
+    case "blog":
+      addLine("Opening Blog...", "color2", 80);
+      newTab(blog);
+      break;
+    case "let me in":
+      addLine("You're in!", "color2", 80);
+      setTimeout(function() {
+        window.open('https://youtu.be/xMPkgtOxlfo');
+      }, 1000); 
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "?":
+      liner.classList.add("password");
+      pw = true;
+      break;
+    case "projects":
+      loopLines(projects, "color2 margin", 80);
+      break;
+    case "password":
+      addLine("<span class=\"inherit\"> Mate you can do better tha this for sure. Just at least try</span>", "error", 100);
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine('hello@learnsec101.com...', "color2", 80);
+      // newTab(email);
+      break;
+    case "clear":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    case "notes":
+      addLine("Opening Infosec Notes...", "color2", 0);
+      newTab(notes);
+      break;
+    case "linkedin":
+      addLine("Opening LinkedIn...", "color2", 0);
+      newTab(linkedin);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    default:
+      addLine("<span class=\"inherit\">Command not found. To list all commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
