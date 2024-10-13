f512e8b5c3b11c94b97ffd15de33f6623d9bfbe6
KashishOO7 <charayakashish@gmail.com>
2024-10-14 00:08:44 +0530

    Update

--- assets/web terminal/commands.js
+++ assets/web terminal/commands.js
@@ -1,17 +1,12 @@
-var blog = "https://medium.com/@kashishcharaya";
-var notes = "https://notes.learnsec101.com/untitled/welcome/intro";
-// Don't look here mate
+// Don't look here mate, Just Don't
 var password = "wargames";
-var linkedin = "https://www.linkedin.com/in/kashish-charaya-/";
-var github = "https://github.com/KashishOO7";
-var showcase = "https://learnsec101.com/assets/web%20terminal/showcase.html";
 
 whois = [
   "<br>",
   "Hey,",
   "I'm just another Human born on the planet earth who thought life was absurd.",
-  "But I created this site for fun and also added a mini CTF challenge.",
-  "So, see you in the end.",
+  "But I created this site for fun",
+  "So, see you at the end.",
   "<br>"
 ];
 
@@ -21,18 +16,9 @@ whoami = [
   "<br>"
 ];
 
-social = [
-  "<br>",
-  'Blog        <a href="' + blog + '" target="_blank">' + "</a>",
-  'Notes        <a href="' + notes + '" target="_blank">' + '</a>',
-  'Linkedin       <a href="' + linkedin + '" target="_blank">' + "</a>",
-  'Github         <a href="' + github + '" target="_blank">' + "</a>",
-  "<br>"
-];
-
 secret = [
   "<br>",
-  '<span class="command">A hint</span>           Type: "let me in"',
+  '<span class="command">A hint</span>           Type: "showme"',
   "<br>"
 ];
 
@@ -42,127 +28,31 @@ projects = [
   "<br>"
 ];
 
-ctf = [
-  "<br>",
-  '<span class="command">whatis</span>         About this CTF?',
-  '<span class="command">howis</span>          How to play?',
-  '<span class="command">levels</span>         Depth of this CTF?',
-  "<br>",
-];
-
-levels = [
-  "<br>",
-  '<span class="command">level1</span>          Noob Level stuff',
-  '<span class="command">level2</span>          Experienced level stuff',
-  '<span class="command">level3</span>          Some Hard level shit',
-  "<br>"
-]
-
-level1 = [
-  "<br>",
-  "----------An intergalactic marriage----------",
-  '<span class="command">hints1</span>         Some hints',
-  '<span class="command">ql1</span>            Listing Challenges',
-  '<span class="command">showcase</span>       Images for question 2 & 3',
-  "<br>",
-]
-ql1 = [
-  "<br>",
-  '<span class="command">que1</span>           help, I forgot!!',
-  '<span class="command">que2</span>           thoughts I have been recalling!',
-  '<span class="command">que3</span>           help me to unrap the Gift for my friend!',
-  "<br>",
-];
-
-whatis = [
-  "<br>",
-  "It's a mini CTF based around encryption, Steganography, cryptography, etc...",
-  "<br>"
-];
-
-music = [
-  "<br>",
-  "If you get irritated or distracted by the music then just mute this tab",
-  "It's Mr. Robot series's music",
-  "<br>"
-]
-
-howis = [
-  "<br>",
-  "The answer you have been seeking has always been hiding in the question.",
-  "No need to use any automated tools OR bruteforcing of any type.",
-  "<br>"
-];
-
-hints1 = [
-  "<br>",
-  "Important stuff: name, numbers, addition(name, numbers(+numbers))",
-  "Once I saw sam hacking 3 intergalactic highly secured website in a bar just by",
-  "examining the page source. Really? Who tf can do that?",
-  "<br>"
-]
-
-que1 = [
-  "<br>",
-  "Que-1: An alien named WXpKR2RFbElVbTlhVTBKb1lrZHNiR0puUFQwPQ== visited earth recently to attend a",
-  "friends wedding, but he forgot his name and without knowing his name guards won't let him enter the venue.",
-  "Find his name. He especially came for gulab jaamun and rabdi. So, help him please. He's our guest.",
-  // he has been recursively forgetting his name, he also had a habit of repeating everything twice in order to recall easily.
-  "<br>"
-];
-
-que2 = [
-  "<br>",
-  "Que-2: After looking through the old messages of his friends, he found his name but also found his",
-  "Childhood picture and recalls the old days with his bestfriend. Help him recover his memories.",  
-  // sam's friend who's getting married has a special alien talent, he can rotate his head 360 degrees
-  // and sam can rotate only 47 degrees but both respect each others talent and always add values to
-  // each others lives.
-  "<br>"
-];
-
-que3 = [
-  "<br>",
-  "Que-3: A parking only for aliens! What does this mean.",
-  // oh man are you a baby? still playing with random numbers!
-  // OR maybe you should because numbers are always important.
-  "<br>"
-];
-
-level2 = [
-  "<br>",
-  "----------Lost Crypto Art Gallery----------",
-  '<span class="command">hints2</span>         Some hints',
-  '<span class="command">ql2</span>            Listing Challenges',
-  '<span class="command">gallery</span>        The crypto art gallery',
-  "<br>",
-];
-
-ql2 = [
-  "<br>",
-  "under construction",
-  "<br>",
-];
-
-que4 = [
-  "<br>",
-  "Que-4: Sam called her wife to share his happiness but her voice was distorted, perhaps",
-  "something is wrong. Help him understand what's wrong.",
-  "<br>"
-];
-
 help = [
   "<br>",
-  '<span class="command">music</span>          Stop Music/Know about music',
-  '<span class="command">whois</span>          Who is Kashish?',
-  '<span class="command">whoami</span>         Who are you?',
-  '<span class="command">social</span>         Display available socials',
-  '<span class="command">?</span>              World\'s first hacking movie?',
-  '<span class="command">ctf</span>            Play a mini CTF',
-  '<span class="command">projects</span>       View some projects',
-  '<span class="command">history</span>        View command history',
-  '<span class="command">help</span>           That\'s what you typed',
-  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">linux</span>        Is this Linux?',
+  '<span class="command">whois</span>        Who is Kashish?',
+  '<span class="command">whoami</span>       Who are you?',
+  '<span class="command">projects</span>     View some projects',
+  '<span class="command">history</span>      View command history',
+  '<span class="command">help</span>         Get some serious help',
+  '<span class="command">clear</span>        Clear terminal',
+  "<br>",
+];
+
+linux = [
+  "<br>",
+  "This is not technically an Linux Distro But But, to see if some commands work, try them out!",
+  "<br>",
+  '<span class="command">pwd</span>            Print working directory',
+  '<span class="command">cd</span>             Change directory',
+  '<span class="command">ls</span>             List files in the current directory',
+  '<span class="command">cat</span>            Read the contents of a file',
+  '<span class="command">top</span>            Display Linux processes',
+  '<span class="command">df</span>             Checking disk space? Not with this command!',
+  '<span class="command">du</span>             Disk usage?',
+  '<span class="command">ifconfig</span>       Network configuration? Not here!',
+  '<span class="command">ping</span>           Pinging? This isn\'t Linux!',
   "<br>",
 ];
 
--- assets/web terminal/main.js
+++ assets/web terminal/main.js
@@ -18,7 +18,7 @@ window.addEventListener("keyup", enterKey);
 
 console.log(
   "%cSecurity Breached!",
-  "color: #04ff00; font-weight: bold; font-size: 24px;"
+  "color: #04ff00; font-weight: bold; font-size: 26px;"
 );
 console.log("%cPassword: '" + password + "' - Just Hack it?", "color: grey");
 
@@ -26,6 +26,17 @@ textarea.value = "";
 command.innerHTML = textarea.value;
 
 function enterKey(e) {
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Tab") {
+        e.preventDefault();
+        const typedCommand = command.innerHTML.trim();
+        const matches = availableCommands.filter(c => c.startsWith(typedCommand));
+        if (matches.length === 1) {
+            command.innerHTML = matches[0];
+        }
+    }
+});
+
   if (e.keyCode == 181) {
     document.location.reload(true);
   }
@@ -76,12 +87,95 @@ function enterKey(e) {
   }
 }
 
-// function openWebpage(url) {
-//   window.open(url, "_blank");
-// }
-
 function commander(cmd) {
-  switch (cmd.toLowerCase()) {
+  const input = cmd.split(' ');
+  const command = input[0];
+  const argument = input[1];
+
+  cmd = cmd.trim().toLowerCase();
+
+  if (cmd.includes("ls -la")) {
+    loopLines([
+      '  ',
+      'drwxr-xr-x  10 guest guest 4096 Oct 12 12:34 .',
+      'drwxr-xr-x  12 guest guest 4096 Oct 12 12:34 ..',
+      '-rw-r--r--   1 guest guest   123 Oct 12 12:34 .profile',
+      '  ',
+      'Looks like you’ve got nothing but dreams in here.'
+    ], "color2 margin", 80);
+    return;
+  }
+
+  if (cmd.includes("ps aux")) {
+    loopLines(['USER       PID %CPU %MEM COMMAND',
+      'guest      1337  99.0 100.0 /usr/bin/fun',
+      '  ',
+      'Oops, I mixed up your processes with a game!'], "color2 margin", 80);
+    return;
+  }
+
+  if (cmd.includes("df -h")) {
+    loopLines([
+      '  ',
+      'Filesystem      Size  Used Avail Use%',
+      '  ',
+      'Not a byte of data is real. Infinite space available!'
+      
+    ], "color2 margin", 80);
+    return;
+  }
+  
+    if(cmd.includes("uname -a")) {
+      loopLines([
+        '  ',
+        'Linux guest-vm 5.4.0-42-generic #46-Ubuntu',
+        'You’re running on Pure Imagination OS, kernel version 42.0, built in the year 3000!',
+        '  ',
+      ], "color2 margin", 80);
+      return;
+    }
+
+  if (cmd.includes("rm -rf /")) {
+    loopLines([
+      '  ',
+      'Warning: You are about to delete everything!',
+      '  ',
+    ], "color2 margin", 80);
+    return;
+  }
+
+  switch (command.toLowerCase()) {
+    case "pwd":
+      addLine(currentDirectory, "color2 margin", 80);
+      break;
+
+    case "ls":
+      const path = argument ? `${currentDirectory}/${argument}` : currentDirectory;
+      const listing = handleLs(path);
+      addLine(listing, "color2 margin", 80);
+      break;
+
+    case "cd":
+      if (argument) {
+        changeDirectory(argument);
+      } else {
+        currentDirectory = '/home/user';
+        addLine(`Changed directory to ${currentDirectory}`, "color2 margin", 80);
+      }
+      break;
+
+    case "cat":
+      if (argument) {
+        const fileContent = handleCat(argument);
+        if (fileContent) {
+          addLine(fileContent, "color2 margin", 80);
+          break;
+        } else {
+          addLine("File not found", "error", 80);
+        }
+      }
+      break;
+
     case "help":
       loopLines(help, "color2 margin", 80);
       break;
@@ -91,119 +185,155 @@ function commander(cmd) {
     case "whoami":
       loopLines(whoami, "color2 margin", 80);
       break;
-    case "blog":
-      addLine("Opening Blog...", "color2", 80);
-      newTab(blog);
-      break;
-    case "let me in":
+    
+    case "showme":
       addLine("You're in!", "color2", 80);
       setTimeout(function() {
         window.open('https://youtu.be/xMPkgtOxlfo');
       }, 1000); 
       break;
-      case "hfgke4":
-      addLine("Here you go!", "color2", 80);
-      setTimeout(function() {
-        window.open('https://youtu.be/dQw4w9WgXcQ?si=HfmYE9jgmWmVpSUo');
-      }, 1000); 
+
+    case "quote":
+      const quotes = [
+        `So much internet money everywhere now you just have to grab it!`,
+        `Congrats! You will receive an email shortly with a survey. Please be sure to fill it out completely. Don't mind the redirect to login to your bank. That's just to verify you're a human and not a bot.`,
+        `If at first you don't succeed; call it version 1.0.`,
+        `There are 10 types of people in the world: those who understand binary, and those who don’t.`,
+        `I’m not great at the advice. Can I interest you in a sarcastic comment?`,
+        `It works on my machine...`,
+        `To err is human – and to blame it on a computer is even more so.`,
+        `I'm not lazy, I'm just buffering.`,
+        `A bug in the code is worth two in the specs.`,
+        `Programmers: turning coffee into code since forever.`,
+        `Have you tried turning it off and on again?`,
+        `404: Inspiration not found.`,
+        `First, solve the problem. Then, write the code.`,
+        `Artificial intelligence is no match for natural stupidity.`,
+        `In case of fire: git commit, git push, leave building.`,
+        `My code doesn’t have bugs, it just develops random features.`,
+        `I’m not a great programmer; I’m just a good programmer with great habits.`,
+        `There’s no place like 127.0.0.1.`,
+        `I don’t always test my code, but when I do, I do it in production.`,
+        `Debugging: Being the detective in a crime movie where you are also the murderer.`,
+        `Code is like humor. When you have to explain it, it’s bad.`,
+        `Programming is like writing a book... except when you miss a single comma on page 126, the whole thing makes no sense.`
+      ];
+    
+      function wrapText(text, maxLength) {
+        const words = text.split(' ');
+        let lines = [];
+        let currentLine = '';
+    
+        words.forEach(word => {
+          if ((currentLine + word).length <= maxLength) {
+            currentLine += (currentLine ? ' ' : '') + word;
+          } else {
+            lines.push(currentLine);
+            currentLine = word;
+          }
+        });
+        
+        if (currentLine) {
+          lines.push(currentLine);
+        }
+    
+        return lines.join('\n');
+      }
+    
+      const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    
+      const wrappedQuote = wrapText(randomQuote, 80);
+    
+      wrappedQuote.split('\n').forEach(line => {
+        addLine(line, "color2", 80);
+      });
       break;
-      case "ctf":
-      loopLines(ctf, "color2 margin", 80);
-      break;
-      case "whatis":
-      loopLines(whatis, "color2 margin", 80);
-      break;
-      case "howis":
-      loopLines(howis, "color2 margin", 80);
-      break;
-      case "hints1":
-      loopLines(hints1, "color2 margin", 80);
-      break;
-      case "levels":
-      loopLines(levels, "color2 margin", 80);
-      break;
-      case "level1":
-      loopLines(level1, "color2 margin", 80);
-      break;
-      case "level2":
-      loopLines(level2, "color2 margin", 80);
-      break;
-      case "level3":
-      loopLines(level3, "color2 margin", 80);
-      break;
-      case "que1":
-      loopLines(que1, "color2 margin", 80);
-      break;
-      case "que2":
-      loopLines(que2, "color2 margin", 80);
-      break;
-      case "que3":
-      loopLines(que3, "color2 margin", 80);
-      break;
-      case "ql1":
-      loopLines(ql1, "color2 margin", 80);
-      break;
-      case "ql2":
-      loopLines(ql2, "color2 margin", 80);
-      break;
-      case "que4":
-      loopLines(que4, "color2 margin", 80);
-      break;
-      case "que5":
-      loopLines(que5, "color2 margin", 80);
-      break;
-      case "que6":
-      loopLines(que6, "color2 margin", 80);
-      break;
-      case "music":
-      loopLines(music, "color2 margin", 80);
-      break;
-    case "social":
-      loopLines(social, "color2 margin", 80);
-      break;
+    
     case "?":
       liner.classList.add("password");
       pw = true;
       break;
+
     case "projects":
       loopLines(projects, "color2 margin", 80);
       break;
+
     case "password":
       addLine("<span class=\"inherit\"> Mate you can do better tha this for sure. Just at least try</span>", "error", 100);
       break;
+
     case "history":
       addLine("<br>", "", 0);
       loopLines(commands, "color2", 80);
       addLine("<br>", "command", 80 * commands.length + 50);
       break;
-      break;
+
     case "clear":
       setTimeout(function() {
         terminal.innerHTML = '<a id="before"></a>';
         before = document.getElementById("before");
       }, 1);
       break;
-    case "banner":
-      loopLines(banner, "", 80);
+
+      case "sudo":
+      loopLines([
+        '[sudo] password for guest: ',
+        '  ',
+        'Nice try, you don’t have superuser privileges. Stick to being a guest.'
+      ], "color2 margin", 80);
       break;
-    case "notes":
-      addLine("Opening Infosec Notes...", "color2", 0);
-      newTab(notes);
+
+      case "top":
+      loopLines([
+        'Monitoring system resources...',
+        'CPU: 99% chill | Memory: 100% imagination | Processes: Too many to count!'
+      ], "color2 margin", 80);
+      break; 
+
+      case "ifconfig":
+        loopLines([
+          'eth0      Link encap:Ethernet  HWaddr 00:1A:2B:3C:4D:5E',
+          'inet addr:192.168.0.101  Bcast:192.168.0.255  Mask:255.255.255.0',
+          '  ',
+          'Status: Connected to a parallel universe!'
+        ], "color2 margin", 80);
+        break;
+
+      case "ping":
+        loopLines([
+          'pinging moon (240.0.0.1) with 64 bytes of data...',
+          '64 bytes from moon: icmp_seq=1 ttl=128 time=384.400 ms',
+          '64 bytes from moon: icmp_seq=2 ttl=128 time=385.200 ms',
+          '64 bytes from moon: icmp_seq=3 ttl=128 time=383.900 ms',
+          '64 bytes from moon: icmp_seq=4 ttl=128 time=384.100 ms',
+          '  ',
+          '--- moon ping statistics ---',
+          '4 packets transmitted, 4 received, 0% packet loss, time 4000ms',
+          'rtt min/avg/max/mdev = 383.900/384.400/385.200/0.500 ms',
+          'Approximate distance: 1.28 light years',
+          'Mission accomplished! The moon is online.'
+        ], "color2 margin", 80);
+        break;
+
+      case "acknowledged":
+        setTimeout(function() {
+          window.open('https://youtu.be/dQw4w9WgXcQ');
+        }, 1000);
+        break;
+
+      case "linux":
+        loopLines(linux, "color2 margin", 80);
+        break;
+
+      case "id":
+      loopLines([
+        '<span class="command">uid=1000(user) gid=1000(user) groups=1000(user),27(sudo)</span>',
+        'Just kidding! This isn\'t Linux, remember?'
+      ], "color2 margin", 80);
       break;
-    case "linkedin":
-      addLine("Opening LinkedIn...", "color2", 0);
-      newTab(linkedin);
-      break;
-    case "github":
-      addLine("Opening GitHub...", "color2", 0);
-      newTab(github);
-      break;
-    case "showcase":
-      addLine("Opening Images...", "color2", 0);
-      newTab(showcase);
-    break;
+      
     default:
-      addLine("<span class=\"inherit\">Command not found. To list all commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
+      addLine("Command not found. Type 'help' for available commands.", "error", 100);
       break;
   }
 }
@@ -239,4 +369,82 @@ function loopLines(name, style, time) {
   name.forEach(function(item, index) {
     addLine(item, style, index * time);
   });
+}
+
+function simulateFileSystem() {
+  return {
+    'home': {
+      'user': {
+        'documents': {
+          'notes.txt': "World's first hacking movie?",
+          'answer.txt': "If you know the answer, type '?' in the terminal > enter > your answer.",
+          'secret.txt': "Wanna know a secret? Type 'acknowledged' in the terminal."
+        }
+      }
+    }
+  };
+}
+
+const fileSystem = simulateFileSystem();
+let currentDirectory = '/home/user';
+
+function handleLs(path) {
+  const parts = path.split('/').filter(Boolean);
+  let currentDir = fileSystem;
+
+  for (let part of parts) {
+    if (currentDir[part]) {
+      currentDir = currentDir[part];
+    } else {
+      return "Directory not found";
+    }
+  }
+
+  if (typeof currentDir === 'object') {
+    return Object.keys(currentDir).join('\n');
+  } else {
+    return "Not a directory";
+  }
+}
+
+function changeDirectory(dir) {
+  const path = dir.startsWith('/') ? dir : `${currentDirectory}/${dir}`;
+  const parts = path.split('/').filter(Boolean);
+  let currentDir = fileSystem;
+
+  for (let part of parts) {
+    if (currentDir[part]) {
+      currentDir = currentDir[part];
+    } else {
+      addLine("Directory not found", "error", 80);
+      return;
+    }
+  }
+
+  if (typeof currentDir === 'object') {
+    currentDirectory = '/' + parts.join('/');
+    addLine(`Changed directory to ${currentDirectory}`, "color2 margin", 80);
+  } else {
+    addLine("Not a directory", "error", 80);
+  }
+}
+
+function handleCat(file) {
+  const path = `${currentDirectory}/${file}`;
+  const parts = path.split('/').filter(Boolean);
+  let currentDir = fileSystem;
+
+  for (let part of parts) {
+    if (currentDir[part]) {
+      currentDir = currentDir[part];
+    } else {
+      return null; 
+    }
+  }
+
+  if (typeof currentDir === 'string') {
+    return currentDir;
+  } else {
+    return null;
+  }
 }
